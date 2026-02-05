3ed03bcc8ed2824a8f0514bfa2f8a354773c0ff9
bibeknayak07 <nayakbibek409@gmail.com>
2026-02-05 18:38:14 +0545

    update

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors'); 
-const bcrypt = require('bcryptjs'); 
-require('dotenv').config();
 
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors'); 
+// const bcrypt = require('bcryptjs'); 
+// require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
